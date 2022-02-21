c28b11fd1cc386924d1f4884857f4fe91e8d6cf6
maudetes <maudet.estelle@gmail.com>
2022-02-21 15:31:46 +0100

    Remove udata_search_service import from __init__

--- udata_search_service/__init__.py
+++ udata_search_service/__init__.py
@@ -1,26 +1,5 @@
-from flask import Flask
-from udata_search_service.config import Config
-from udata_search_service.container import Container
-from udata_search_service.presentation import api, commands
+'''
+udata search service
+'''
 
-
-__version__ = '1.0.0'
-
-
-def create_app(config: object = Config) -> Flask:
-    container = Container()
-    container.wire(modules=[api, commands])
-
-    app: Flask = Flask(__name__)
-    app.container = container
-    app.config.from_object(config)
-
-    container.config.elasticsearch_url.from_value(app.config['ELASTICSEARCH_URL'])
-    container.config.search_synonyms.from_value(app.config['SEARCH_SYNONYMS'])
-
-    # register the database command
-    commands.init_app(app)
-
-    app.register_blueprint(api.bp)
-
-    return app
+__version__ = '0.0.0'
---
+++ udata_search_service/app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+from udata_search_service.config import Config
+from udata_search_service.container import Container
+from udata_search_service.presentation import api, commands
+
+
+def create_app(config: object = Config) -> Flask:
+    container = Container()
+    container.wire(modules=[api, commands])
+
+    app: Flask = Flask(__name__)
+    app.container = container
+    app.config.from_object(config)
+
+    container.config.elasticsearch_url.from_value(app.config['ELASTICSEARCH_URL'])
+    container.config.search_synonyms.from_value(app.config['SEARCH_SYNONYMS'])
+
+    # register the database command
+    commands.init_app(app)
+
+    app.register_blueprint(api.bp)
+
+    return app
