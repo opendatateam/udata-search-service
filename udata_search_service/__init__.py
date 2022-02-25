49ab224ad399f907c3c87b67500bd99ea5b3922c
maudetes <maudet.estelle@gmail.com>
2022-02-25 15:24:57 +0100

    Try out dynamic version

--- udata_search_service/__init__.py
+++ udata_search_service/__init__.py
@@ -2,4 +2,9 @@
 udata search service
 '''
 
-__version__ = '0.0.0.dev1'
+import os
+
+tag = os.environ.get('CIRCLE_TAG')
+build_num = os.environ.get('CIRCLE_BUILD_NUM')
+
+__version__ = '0.0.0' + (build_num if not tag and build_num else '')
